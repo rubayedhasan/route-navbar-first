a8a1d22bd79d39c647795cceeb1f6cd6cda725ce
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-11-18 21:10:18 +0600

    add menu icon and condition to toggle the menu icon with cross icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import NavLink from "../NavLink/NavLink";
+import { RiMenu2Fill } from "react-icons/ri";
+import { RxCross2 } from "react-icons/rx";
 
 const NavBar = () => {
+  // state: to toggle the menu icon
+  const [openMenu, setOpenMenu] = useState(false);
+
   // fake routes
   const routes = [
     { id: 1, path: "/", name: "Home" },
@@ -11,8 +17,15 @@ const NavBar = () => {
   ];
 
   return (
-    <nav>
+    <nav className="text-2xl md:hidden">
+      {/* hamburger icons container  */}
+      <div onClick={() => setOpenMenu(!openMenu)}>
+        {openMenu === true ? <RxCross2 /> : <RiMenu2Fill />}
+      </div>
+
+      {/* nav items container  */}
       <ul className="md:flex">
+        {/* nav items  */}
         {routes.map((route) => (
           <NavLink key={route?.id} route={route}></NavLink>
         ))}
